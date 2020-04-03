9b1ee0bb1176b56a2a74f4e34e9c293029327e35
Jonathan <jonathan@evangelchurch.com>
2020-04-03 12:52:33 -0700

    utility class

--- utilities/menu.py
+++ utilities/menu.py
@@ -30,3 +30,61 @@ def menu_option(stdscr, string, display_y, display_x, cursor_y):
     stdscr.addstr(display_y, display_x, string)
     if cursor_y == display_y:
         stdscr.attroff(curses.color_pair(3))
+
+def input_option(stdscr, string, display_y, display_x, cursor_y, isEmpty):
+    if cursor_y == display_y:
+        if isEmpty:
+            stdscr.attron(curses.color_pair(2))
+        else:
+            stdscr.attron(curses.color_pair(1))
+    stdscr.addstr(display_y, display_x, string)
+    if cursor_y == display_y:
+        if isEmpty:
+            stdscr.attroff(curses.color_pair(2))
+        else:
+            stdscr.attroff(curses.color_pair(1))
+
+def add_label(stdscr, string, display_y, display_x):
+    stdscr.addstr(display_y, display_x, string)
+
+def add_solid(stdscr, string, display_y, display_x, cursor_y):
+    if display_y == cursor_y:
+        stdscr.attron(curses.color_pair(3))
+    stdscr.addstr(display_y, display_x, string)
+    if display_y == cursor_y:
+        stdscr.attroff(curses.color_pair(3))
+
+def add_string(stdscr, string, display_y, display_x):
+    stdscr.addstr(display_y, display_x, string)
+
+def status(stdscr, isError, success, display_y, error_color, success_color):
+    width = get_width(stdscr)
+    if isError:
+        stdscr.attron(error_color)
+    else:
+        stdscr.attron(success_color)
+    stdscr.addstr(display_y, (width - len(success)) // 2, success)
+    if isError:
+        stdscr.attroff(error_color)
+    else:
+        stdscr.attroff(success_color)
+
+def inputs(text, k, x_pos_max, cursor_x):
+    char = str(chr(k))
+    if char.isnumeric():
+        if not cursor_x == x_pos_max + len(text):
+            s_pos = cursor_x - x_pos_max
+            temp = list(text)
+            temp[s_pos] = char
+            text = "".join(temp)
+    return text
+
+def time_markers(isEnabled, display_y, display_x, stdscr):
+    if isEnabled:
+        stdscr.attron(curses.color_pair(4))
+        stdscr.addstr(display_y, display_x, "TRUE")
+        stdscr.attroff(curses.color_pair(4))
+    else:
+        stdscr.attron(curses.color_pair(2))
+        stdscr.addstr(display_y, display_x, "FALSE")
+        stdscr.attroff(curses.color_pair(2))
